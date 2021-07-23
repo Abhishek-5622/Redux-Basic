c1e131707ea66f27865e6eb636104c7e5734b30e
Abhishek Mehta <abhishekmehta765@gmail.com>
2021-07-23 21:47:46 +0530

    Redux Demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import store from './redux/store';
+import {Provider} from 'react-redux';
+import BallContainer from './BallContainer';
+import BatContainer from './BatContainer';
+import UserContainer from './UserContainer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Provider store={store} >
+     <div className='App'>
+       {/* <BallContainer/>
+       <BatContainer/> */}
+       <UserContainer/>
+     </div>
+
+   </Provider>
   );
 }
 
